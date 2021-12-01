103e3c2e0ae1f2f226f8b4d363e7d04712a4b721
rimatik <slaven@rimatik.hr>
2021-12-01 17:59:29 +0100

    removed redundant user,recipient,amount params from functions

--- backend/hardhat/contracts/HydraMIddleware.sol
+++ backend/hardhat/contracts/HydraMIddleware.sol
@@ -8,23 +8,23 @@ import "./interfaces/hop/IL1_Bridge.sol";
 
 contract XsnBridgeAggregator {
 
-    function bridgePoly(address predicate, address contractAddr, address user, address rootToken, uint256 amount, bytes calldata depositData) external {
+      function bridgePoly(address predicate, address contractAddr, address rootToken, uint256 amount, bytes calldata depositData) external {
         IERC20(rootToken).approve(predicate,amount);
         IERC20(rootToken).transferFrom(msg.sender,address(this),amount);
-        IRootChainManager(contractAddr).depositFor(user,rootToken,depositData);
+        IRootChainManager(contractAddr).depositFor(msg.sender,rootToken,depositData);
     }
 
-     function bridgeEthPoly(address contractAddr, address user) payable external {
-        IRootChainManager(contractAddr).depositEtherFor{value: msg.value}(user);
+     function bridgeEthPoly(address contractAddr) payable external {
+        IRootChainManager(contractAddr).depositEtherFor{value: msg.value}(msg.sender);
     }
 
-    function sendToL2Hop(address rootToken, address contractAddr, uint256 chainId, address recipient,uint256 amount,uint256 amountOutMin,uint256 deadline,address relayer,uint256 relayerFee) external {
+    function sendToL2Hop(address rootToken, address contractAddr, uint256 chainId,uint256 amount,uint256 amountOutMin,uint256 deadline,address relayer,uint256 relayerFee) external {
         IERC20(rootToken).approve(contractAddr,amount);
         IERC20(rootToken).transferFrom(msg.sender,address(this),amount);
-        IL1_Bridge(contractAddr).sendToL2(chainId,recipient,amount,amountOutMin,deadline,relayer,relayerFee);
+        IL1_Bridge(contractAddr).sendToL2(chainId,msg.sender,amount,amountOutMin,deadline,relayer,relayerFee);
     }
 
-    function sendToL2HopEth(address contractAddr, uint256 chainId, address recipient,uint256 amount,uint256 amountOutMin,uint256 deadline,address relayer,uint256 relayerFee) payable external {
-        IL1_Bridge(contractAddr).sendToL2{value:msg.value}(chainId,recipient,amount,amountOutMin,deadline,relayer,relayerFee);
+    function sendToL2HopEth(address contractAddr, uint256 chainId,uint256 amountOutMin,uint256 deadline,address relayer,uint256 relayerFee) payable external {
+        IL1_Bridge(contractAddr).sendToL2{value:msg.value}(chainId,msg.sender,msg.value,amountOutMin,deadline,relayer,relayerFee);
     }
 }
