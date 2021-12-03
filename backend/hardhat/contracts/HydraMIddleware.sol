fd8a4ece8b51cd4096f2a8671ffc671e85c83604
rimatik <slaven@rimatik.hr>
2021-12-03 16:08:24 +0100

    added common variables and methods for setting them

--- backend/hardhat/contracts/HydraMIddleware.sol
+++ backend/hardhat/contracts/HydraMIddleware.sol
@@ -3,28 +3,83 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "./interfaces/polygon/IRootChainManager.sol";
 import "./interfaces/hop/IL1_Bridge.sol";
 
-contract XsnBridgeAggregator {
+contract HydraMIddleware is Ownable, Pausable {
+    /**
+        Address of polygon bridge root chain manager contract
+    */
+     address _polygonRootChainManager;
 
-      function bridgePoly(address predicate, address contractAddr, address rootToken, uint256 amount, bytes calldata depositData) external {
-        IERC20(rootToken).approve(predicate,amount);
+     /**
+        Address of polygon bridge erc20Predicate contract
+    */
+
+     address _erc20Predicate;
+
+     /**
+        Address of hop bridge contract
+    */
+     address _hopBridge;
+
+    constructor(address polygonRootChainManager, address erc20Predicate, address hopBridge) {
+         _polygonRootChainManager = polygonRootChainManager;
+          _erc20Predicate = erc20Predicate;
+          _hopBridge = hopBridge;
+    }
+
+     function sendToPolygon(address recipient, address rootToken, uint256 amount, bytes calldata depositData) external {
+        IERC20(rootToken).approve(_erc20Predicate,amount);
         IERC20(rootToken).transferFrom(msg.sender,address(this),amount);
-        IRootChainManager(contractAddr).depositFor(msg.sender,rootToken,depositData);
+        IRootChainManager(_polygonRootChainManager).depositFor(recipient,rootToken,depositData);
     }
 
-     function bridgeEthPoly(address contractAddr) payable external {
-        IRootChainManager(contractAddr).depositEtherFor{value: msg.value}(msg.sender);
+     function sendEthToPolygon( address recipient) payable external {
+        IRootChainManager(_polygonRootChainManager).depositEtherFor{value: msg.value}(recipient);
     }
 
-    function sendToL2Hop(address rootToken, address contractAddr, uint256 chainId,uint256 amount,uint256 amountOutMin,uint256 deadline,address relayer,uint256 relayerFee) external {
-        IERC20(rootToken).approve(contractAddr,amount);
+    function sendToL2Hop(address rootToken, address recipient, uint256 chainId,uint256 amount,uint256 amountOutMin,uint256 deadline,address relayer,uint256 relayerFee) external {
+        IERC20(rootToken).approve(_hopBridge,amount);
         IERC20(rootToken).transferFrom(msg.sender,address(this),amount);
-        IL1_Bridge(contractAddr).sendToL2(chainId,msg.sender,amount,amountOutMin,deadline,relayer,relayerFee);
+        IL1_Bridge(_hopBridge).sendToL2(chainId,recipient,amount,amountOutMin,deadline,relayer,relayerFee);
     }
 
-    function sendToL2HopEth(address contractAddr, uint256 chainId,uint256 amountOutMin,uint256 deadline,address relayer,uint256 relayerFee) payable external {
-        IL1_Bridge(contractAddr).sendToL2{value:msg.value}(chainId,msg.sender,msg.value,amountOutMin,deadline,relayer,relayerFee);
+    function sendEthToL2Hop(address recipient, uint256 chainId,uint256 amountOutMin,uint256 deadline,address relayer,uint256 relayerFee) payable external {
+        IL1_Bridge(_hopBridge).sendToL2{value:msg.value}(chainId,recipient,msg.value,amountOutMin,deadline,relayer,relayerFee);
     }
+
+     function checkAllowance(address owner, address spender) external view returns (uint256){
+         return IERC20(address(this)).allowance(owner,spender);
+     }
+
+    function setRootManager(address polygonRootChainManager) external onlyOwner { 
+        _polygonRootChainManager = polygonRootChainManager;
+    }
+
+    function setHopBridge(address hopBridge) external onlyOwner { 
+          _hopBridge = hopBridge;
+    }
+
+     function setErc20Predicate(address erc20Predicate) external onlyOwner { 
+         _erc20Predicate = erc20Predicate;
+    }
+
+    /**
+        Disable the contract.
+    */
+    function pause() external onlyOwner whenNotPaused {
+        _pause();
+    }
+
+    /**
+        Enable the contract.
+    */
+    function unpause() external onlyOwner whenPaused {
+        _unpause();
+    }
+
+   
 }
