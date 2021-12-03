bbea05f208a3e057102e1cecb7ee6b0febd77189
rimatik <slaven@rimatik.hr>
2021-12-03 16:05:39 +0100

    added dtos.ts and enums

---
+++ backend/api/common/dtos.ts
@@ -0,0 +1,47 @@
+import { Asset, ChainId, Route } from "./enums";
+
+export interface CheckAllowanceDto {
+  chainId: string;
+  owner: string;
+  spender: string;
+  tokenAddress: string;
+}
+
+export interface BuildAllowanceRequestDto {
+  chainId: string;
+  owner: string;
+  spender: string;
+  tokenAddress: string;
+  amount: string;
+}
+
+export interface BuildAllowanceResponseDto {
+  data: string;
+  to: string;
+  from: string;
+}
+
+export interface QuoteRequestDto {
+  fromAsset: Asset;
+  fromChainId: string;
+  toAsset: Asset;
+  toChainId: string;
+  amount: string;
+}
+
+export interface BuildTxRequestDto {
+  recipient: string;
+  fromAsset: Asset;
+  fromChainId: ChainId;
+  toAsset: Asset;
+  toChainId: ChainId;
+  amount: string;
+}
+
+export interface PolygonBridgeDto {
+  erc20Predicate?: string;
+  contractAddr?: string;
+  rootToken?: string;
+  amount?: string;
+  depositData?: string;
+}
---
+++ backend/api/common/enums.ts
@@ -0,0 +1,14 @@
+export enum Route {
+    Polygon,
+    Hop
+}
+
+export enum Asset {
+    Eth,
+    Usdc
+}
+
+export enum ChainId {
+    Mainnet,
+    Polygon
+}
