4dccc31cc819dcf1e0bde528fe5040800c00ce6d
rimatik <slaven@rimatik.hr>
2021-12-23 09:59:16 +0100

    changed response to BaseResponseDto

--- frontend/src/api/bridgeService.ts
+++ frontend/src/api/bridgeService.ts
@@ -1,19 +1,28 @@
-import { BaseListResponseDto, BaseResponseDto, BuildTxRequestDto, BuildTxResponseDto, QuoteRequestDto, QuoteResponseDto } from "../common/dtos";
+import {
+  BaseResponseDto,
+  BuildTxRequestDto,
+  BuildTxResponseDto,
+  QuoteRequestDto,
+  QuoteResponseDto,
+} from "../common/dtos";
 import { fetchWrapper } from "../helpers/fetchWrapper";
 
 require("dotenv").config();
 const { REACT_APP_API_URL } = process.env;
 
-export const buildBridgeTx = async (dto: BuildTxRequestDto): Promise<BaseResponseDto<BuildTxResponseDto>> => {
+export const buildBridgeTx = async (
+  dto: BuildTxRequestDto
+): Promise<BaseResponseDto<BuildTxResponseDto>> => {
   const response: any = await fetchWrapper.get(
     `${REACT_APP_API_URL}/bridge/build-tx?recipient=${dto.recipient}&fromAsset=${dto.fromAsset}&fromChainId=${dto.fromChainId}&toAsset=${dto.toAsset}&toChainId=${dto.toChainId}&amount=${dto.amount}&routeId=${dto.routeId}`
   );
+ 
   return response.result.data;
 };
 
 export const getQuote = async (
-  dto : QuoteRequestDto
-): Promise<BaseListResponseDto<QuoteResponseDto>> => {
+  dto: QuoteRequestDto
+): Promise<BaseResponseDto<QuoteResponseDto>> => {
   const response: any = await fetchWrapper.get(
     `${REACT_APP_API_URL}/bridge/quote?fromAsset=${dto.fromAsset}&fromChainId=${dto.fromChainId}&toAsset=${dto.toAsset}&toChainId=${dto.toChainId}&amount=${dto.amount}`
   );
