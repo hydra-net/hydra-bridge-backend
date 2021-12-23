838d2e41d70276cbc8bfc40fc1da49539ebe1e9c
rimatik <slaven@rimatik.hr>
2021-12-23 10:01:10 +0100

    added hop icon

--- frontend/src/common/components/Icon/index.tsx
+++ frontend/src/common/components/Icon/index.tsx
@@ -1,13 +1,15 @@
 import { ReactComponent as arrowRight } from "../../../assets/arrow-right.svg";
 import { ReactComponent as eth } from "../../../assets/ethereum.svg";
-import { ReactComponent as polygon } from "../../../assets/polygon.svg";
 import { ReactComponent as usdc } from "../../../assets/usdc.svg";
+import { ReactComponent as polygon } from "../../../assets/polygon.svg";
+import { ReactComponent as hop } from "../../../assets/hop.svg";
 
 export const icons = {
   arrowRight,
   eth,
-  polygon,
   usdc,
+  polygon,
+  hop
 };
 
 export default icons;
