18e00a605048e923f1602d40fff3a02eec40433f
rimatik <slaven@rimatik.hr>
2021-12-23 09:54:16 +0100

    added mapper helper

---
+++ backend/api/helpers/mappers/mapperDto.ts
@@ -0,0 +1,57 @@
+import { RouteDto, TokenResponseDto } from "../../common/dtos";
+import { Asset } from "../../common/enums";
+
+export const mapTokenToDto = (
+  token: any,
+  chainId: number
+): TokenResponseDto => {
+  return {
+    id: token.id,
+    name: token.name,
+    chainId: chainId,
+    address: token.address,
+    decimals: token.decimals,
+    symbol: token.symbol,
+  };
+};
+
+export const mapRouteToDto = (
+  route: any,
+  allowanceTarget: string,
+  bridge: any,
+  token: TokenResponseDto,
+  chainFromId: number,
+  chainToId: number,
+  amountIn: string,
+  amountOut: string
+): RouteDto => {
+  return {
+    id: route.id,
+    allowanceTarget: allowanceTarget,
+    isApprovalRequired: token.symbol !== Asset.eth.toString(),
+    bridgeRoute: {
+      bridgeName: bridge.name,
+      bridgeId: bridge.id,
+      bridgeInfo: {
+        displayName: bridge.display_name,
+        serviceTime: 0,
+        isTestnet: bridge.is_testnet
+      },
+      fromAsset: token,
+      fromChainId: chainFromId,
+      toAsset: token,
+      toChainId: chainToId,
+      amountIn: amountIn,
+      amountOut: amountOut, //TODO: add function to calculate amountOut
+    },
+    fees: {
+      gasLimit: [
+        {
+          amount: "0", //TODO:calculate gas limit amount
+          assetAddress: token.address,
+          chainId: chainFromId,
+        },
+      ],
+    },
+  };
+};
