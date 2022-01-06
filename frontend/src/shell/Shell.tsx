b6454552623225fbca72d3db95d2f727bf1ee938
rimatik <slaven@rimatik.hr>
2022-01-06 15:18:39 +0100

    added FallbackContainer to Shell

--- frontend/src/shell/Shell.tsx
+++ frontend/src/shell/Shell.tsx
@@ -4,6 +4,9 @@ import { Routes, Route } from "react-router-dom";
 import { defaultTheme } from "./theme/theme";
 import { lazyWithPreload } from "../helpers/lazy";
 import { routes } from "../routes";
+import LoadingSpinner from "../common/components/LoadingSpinner";
+import styled from "styled-components";
+import { getFlexCenter } from "../common/styles";
 
 const Home = lazyWithPreload(
   () => import(/* webpackChunkName: 'LandingModule' */ "../modules/Home/Home")
@@ -13,12 +16,21 @@ const Page404 = lazyWithPreload(
   () =>
     import(/* webpackChunkName: 'LandingModule' */ "../modules/Page404/Page404")
 );
-
+const FallbackContainer = styled.div`
+  ${getFlexCenter};
+  height: 460px;
+`;
 const Shell = () => {
   return (
     <>
       <Layout theme={defaultTheme}>
-        <Suspense fallback={"loading..."}>
+        <Suspense
+          fallback={
+            <FallbackContainer>
+              <LoadingSpinner />
+            </FallbackContainer>
+          }
+        >
           <Routes>
             <Route path={routes.home} element={<Home />} />
             <Route path={routes.page404} element={<Page404 />} />
