ba1397190338a8a85e6035261886645541bd255e
rimatik <slaven@rimatik.hr>
2022-01-07 19:13:15 +0100

    added falback container

--- frontend/src/shell/Shell.tsx
+++ frontend/src/shell/Shell.tsx
@@ -4,9 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import { defaultTheme } from "./theme/theme";
 import { lazyWithPreload } from "../helpers/lazy";
 import { routes } from "../routes";
-import LoadingSpinner from "../common/components/LoadingSpinner";
-import styled from "styled-components";
-import { getFlexCenter } from "../common/styles";
+import Fallback from "./Fallback";
 
 const Home = lazyWithPreload(
   () => import(/* webpackChunkName: 'LandingModule' */ "../modules/Home/Home")
@@ -16,21 +14,12 @@ const Page404 = lazyWithPreload(
   () =>
     import(/* webpackChunkName: 'LandingModule' */ "../modules/Page404/Page404")
 );
-const FallbackContainer = styled.div`
-  ${getFlexCenter};
-  height: 460px;
-`;
+
 const Shell = () => {
   return (
     <>
       <Layout theme={defaultTheme}>
-        <Suspense
-          fallback={
-            <FallbackContainer>
-              <LoadingSpinner />
-            </FallbackContainer>
-          }
-        >
+        <Suspense fallback={<Fallback />}>
           <Routes>
             <Route path={routes.home} element={<Home />} />
             <Route path={routes.page404} element={<Page404 />} />
