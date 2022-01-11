277058662f63f9e9de6f751864efac83309e9cdc
rimatik <slaven@rimatik.hr>
2022-01-11 16:45:18 +0100

    removed step from input

--- frontend/src/shell/theme/globalStyle.ts
+++ frontend/src/shell/theme/globalStyle.ts
@@ -10,4 +10,16 @@ body {
   font-family: 'Roboto', sans-serif;
 }
 
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
+
 `;
