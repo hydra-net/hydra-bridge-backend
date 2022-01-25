7d8fcff8cc593a18fe31fb6db1d025987cf16819
rimatik <slaven@rimatik.hr>
2022-01-25 16:26:04 +0100

    changed seed scripts, added Arbitrum, Optimism

--- backend/prisma/seeds/bridges_tokens.ts
+++ backend/prisma/seeds/bridges_tokens.ts
@@ -1,31 +1,38 @@
 export const bridges_tokens = [
-    {
-        bridge_id : 1, // Polygon 
-        token_id: 1 // ETH
-    },
-    {
-        bridge_id : 1, // Polygon 
-        token_id: 2 // USDC
-    },
-    {
-        bridge_id : 2, // Hop 
-        token_id: 1 // ETH
-    },
-    {
-        bridge_id : 2, // Hop 
-        token_id: 2 // USDC
-    },
-    {
-        bridge_id : 3, // Polygon goerli
-        token_id: 1 // ETH
-    },
-    {
-        bridge_id : 3, // Polygon goerli
-        token_id: 4 // USDC goerli
-    },
-    {
-        bridge_id : 4, // Hop 
-        token_id: 4 // USDC goerli
-    },
-]
-    
+  {
+    bridge_id: 1, // Polygon
+    token_id: 1, // ETH
+  },
+  {
+    bridge_id: 1, // Polygon
+    token_id: 2, // USDC
+  },
+  {
+    bridge_id: 2, // Hop
+    token_id: 1, // ETH
+  },
+  {
+    bridge_id: 2, // Hop
+    token_id: 2, // USDC
+  },
+  {
+    bridge_id: 3, // Polygon goerli
+    token_id: 1, // ETH
+  },
+  {
+    bridge_id: 3, // Polygon goerli
+    token_id: 4, // USDC goerli
+  },
+  {
+    bridge_id: 4, // Hop
+    token_id: 4, // USDC goerli
+  },
+  {
+    bridge_id: 4, // Hop
+    token_id: 7, // USDC arbitrum
+  },
+  {
+    bridge_id: 4, // Hop
+    token_id: 8, // USDC optimism
+  },
+];
--- backend/prisma/seeds/routes.ts
+++ backend/prisma/seeds/routes.ts
@@ -1,50 +1,62 @@
 export const routes = [
-    {
-        // id: 1,
-        chain_from_id: 1, // Mainnet
-        chain_to_id: 2, // Polygon
-        bridge_id: 1
-    },
-    {
-        // id: 2,
-        chain_from_id: 2, // Polygon
-        chain_to_id: 1, // Mainnet,
-        bridge_id: 1
-    },
-    {
-        // id: 3,
-        chain_from_id: 1, // Mainnet
-        chain_to_id: 2, // Polygon
-        bridge_id: 2
-    },
-    {
-        // id: 4,
-        chain_from_id: 2, // Polygon
-        chain_to_id: 1, // Mainnet,
-        bridge_id: 2
-    },
-    {
-        // id: 5,
-        chain_from_id: 3, // Polygon mumbai
-        chain_to_id: 4, // Goerli
-        bridge_id: 3
-    },
-    {
-        // id: 6,
-        chain_from_id: 4, // Goerli
-        chain_to_id: 3, // Poygon mumbai
-        bridge_id: 3
-    },
-    {
-        // id: 7,
-        chain_from_id: 3, // Polygon mumbai
-        chain_to_id: 4, // Goerli
-        bridge_id: 4
-    },
-    {
-        // id: 8,
-        chain_from_id: 4, // Goerli
-        chain_to_id: 3, // Poygon mumbai
-        bridge_id: 4
-    }
-]
+  {
+    // id: 1,
+    chain_from_id: 1, // Mainnet
+    chain_to_id: 2, // Polygon
+    bridge_id: 1,
+  },
+  {
+    // id: 2,
+    chain_from_id: 2, // Polygon
+    chain_to_id: 1, // Mainnet,
+    bridge_id: 1,
+  },
+  {
+    // id: 3,
+    chain_from_id: 1, // Mainnet
+    chain_to_id: 2, // Polygon
+    bridge_id: 2,
+  },
+  {
+    // id: 4,
+    chain_from_id: 2, // Polygon
+    chain_to_id: 1, // Mainnet,
+    bridge_id: 2,
+  },
+  {
+    // id: 5,
+    chain_from_id: 3, // Polygon mumbai
+    chain_to_id: 4, // Goerli
+    bridge_id: 3,
+  },
+  {
+    // id: 6,
+    chain_from_id: 4, // Goerli
+    chain_to_id: 3, // Poygon mumbai
+    bridge_id: 3,
+  },
+  {
+    // id: 7,
+    chain_from_id: 3, // Polygon mumbai
+    chain_to_id: 4, // Goerli
+    bridge_id: 4,
+  },
+  {
+    // id: 8,
+    chain_from_id: 4, // Goerli
+    chain_to_id: 3, // Poygon mumbai
+    bridge_id: 4,
+  },
+  {
+    // id: 9,
+    chain_from_id: 1, // Mainnet
+    chain_to_id: 5, // Arbitrum
+    bridge_id: 2,
+  },
+  {
+    // id: 10,
+    chain_from_id: 1, // Mainnet
+    chain_to_id: 6, // Optimism
+    bridge_id: 2,
+  },
+];
