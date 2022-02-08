49cb7b121d8874b5979909cf93f81e6f10039731
rimatik <slaven@rimatik.hr>
2022-02-08 17:20:23 +0100

    added missing server errors and included ServiceResponse as return type

--- api/helpers/serviceErrorHelper.ts
+++ api/helpers/serviceErrorHelper.ts
@@ -1,5 +1,30 @@
 import { ServiceResponseDto } from "../common/dtos";
 
+export const BadRequest = <T>(
+  message = "Bad request"
+): ServiceResponseDto<T> => {
+  return {
+    status: 400,
+    message: message,
+  };
+};
+
+export const Unauthorized = <T>(
+  message = "Unauthorized"
+): ServiceResponseDto<T> => {
+  return {
+    status: 401,
+    message: message,
+  };
+};
+
+export const Forbidden = <T>(message = "Forbidden"): ServiceResponseDto<T> => {
+  return {
+    status: 403,
+    message: message,
+  };
+};
+
 export const NotFound = <T>(message = "Not found"): ServiceResponseDto<T> => {
   return {
     status: 404,
@@ -7,14 +32,18 @@ export const NotFound = <T>(message = "Not found"): ServiceResponseDto<T> => {
   };
 };
 
-export const BadRequest = (message = "Bad request") => {
+export const Conflict = <T>(
+  message = "Conflict in current state"
+): ServiceResponseDto<T> => {
   return {
-    status: 400,
+    status: 409,
     message: message,
   };
 };
 
-export const ServerError = (message = "Server error") => {
+export const ServerError = <T>(
+  message = "Server error"
+): ServiceResponseDto<T> => {
   return {
     status: 500,
     message: message,
