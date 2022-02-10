2d3982717fb753f1b1dc522e06ceefd4e3057c93
rimatik <slaven@rimatik.hr>
2022-02-10 13:46:58 +0100

    added HOP_RELAYER, HOP_RELAYER_FEE as constants

--- api/common/constants.ts
+++ api/common/constants.ts
@@ -4,3 +4,5 @@ import { hydraBridge } from "../helpers/contractInterfaces/contractInterfaces";
 export const HYDRA_BRIDGE_INTERFACE = new Interface(hydraBridge);
 export const ETHEREUM_NAME = "ethereum";
 export const ETHEREUM_NETWORK_NAME = "mainnet";
+export const HOP_RELAYER = "0x0000000000000000000000000000000000000000";
+export const HOP_RELAYER_FEE = "0";
